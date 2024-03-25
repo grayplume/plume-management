c6e949080205c4694354140eac531596af27dc36
grayplume <birdgray@163,com>
2024-03-25 21:25:07 +0800

    init

---
+++ src/test/java/com/plume/management/PlumeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plume.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
